c4d8386af3535b4af9bcc07507b1272a742057d1
Ding <Desertfeng@gmail.com>
2023-03-08 23:07:59 +1100

    11

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -6,10 +6,11 @@ public class Explosion : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("1");
+      
         if(collision.gameObject.tag == "Enemy")
         {
             collision.gameObject.GetComponent<EnemyAI>().Death();
+            Destroy(this.gameObject);
         }
 
 
